7ca8f7c93e9e06935e413e4c0bb534ece1332a40
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-09 23:50:59 -0300

    FEAT(criar-pensamentos): Criando componente CriarPensamentosComponent

---
+++ memoteca/src/app/componentes/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamentos',
+  templateUrl: './criar-pensamentos.component.html',
+  styleUrls: ['./criar-pensamentos.component.css']
+})
+export class CriarPensamentosComponent {
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: 'modelo1'
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  criarPensamento() {
+    alert("Novo pensamento criado!")
+  }
+
+  cancelar() {
+    alert("Ação cancelada!")
+  }
+}
