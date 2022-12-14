57692ff358fdd9bc99bff4015c4cdd4f7de5cad5
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-13 23:29:13 -0300

    FEAT(pensamento): Alterando serviço de lista e criando serviço de mudarFavorito

--- memoteca/src/app/componentes/pensamentos/pensamento.service.ts
+++ memoteca/src/app/componentes/pensamentos/pensamento.service.ts
@@ -11,12 +11,24 @@ export class PensamentoService {
 
   constructor(private http: HttpClient) {}
 
-  listar(pagina: number): Observable<Pensamento[]> {
+  listar(
+    pagina: number,
+    filtro: string,
+    favoritos: boolean
+  ): Observable<Pensamento[]> {
     const itensPorPagina = 6;
 
     let params = new HttpParams()
       .set('_page', pagina)
       .set('_limit', itensPorPagina);
+
+    if (filtro.trim().length > 2) {
+      params.set('q', filtro);
+    }
+
+    if (favoritos) {
+      params = params.set('favorito', true);
+    }
 
     return this.http.get<Pensamento[]>(this.API, { params });
   }
@@ -38,5 +50,10 @@ export class PensamentoService {
   editar(pensamento: Pensamento): Observable<Pensamento> {
     const url = `${this.API}/${pensamento.id}`;
     return this.http.put<Pensamento>(url, pensamento);
+  }
+
+  mudarFavorito(pensamento: Pensamento): Observable<Pensamento> {
+    pensamento.favorito = !pensamento.favorito;
+    return this.editar(pensamento);
   }
 }
